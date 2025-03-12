df23cac4fc62ef5c4b37d19dabaa65f30122d48f
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-03-13 09:34:49 +1000

    added .gitignore

--- scripts/reconstruction_mapping.py
+++ scripts/reconstruction_mapping.py
@@ -1,19 +1,41 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
+
+# import subprocess
+import urllib.request
 import io
 import pandas as pd
 import argparse
-# from micro_fr_pred.util import clean_emapper_data
-
-# import numpy as np
-# from Bio import SeqIO
+from micro_fr_pred.util import clean_emapper_data
 import requests
 
-eggnog_url = "https://spire.embl.de/download_eggnog/"
-protein_assembly_url = "https://swifter.embl.de/~fullam/spire/genes_per_study/Lloyd-Price_2019_HMP2IBD_spire_v1_genecalls_faa.tar"
-mag_url = "https://swifter.embl.de/~fullam/spire/compiled/Lloyd-Price_2019_HMP2IBD_spire_v1_MAGs.tar"
-study_metadata = "https://spire.embl.de/api/study/Lloyd-Price_2019_HMP2IBD?format=tsv"
-sample_metadata = "https://spire.embl.de/api/sample/SAMN07510031?format=tsv"
-data_folder = "../data/"
+data_folder = "/work/microbiome/users/areiasca/micro_fr_pred/data/"
+
+
+def process_sample(samp):
+    # Get metadata
+    url = requests.get(f"https://spire.embl.de/api/sample/{samp}?format=tsv").text
+    sample_meta = pd.read_csv(io.StringIO(url), sep="\t")
+    print(
+        f"Sample: {samp}\tNo. of MAGs: {len(sample_meta.spire_id)}\nList of MAGS:{sample_meta.spire_id.tolist()}"
+    )
+    # Download EggNOG-mapper data
+    urllib.request.urlretrieve(
+        f"https://spire.embl.de/download_eggnog/{samp}",
+        f"{data_folder}{samp}/emapper_annotations.gz",
+    )
+    eggnog_data = clean_emapper_data(f"{data_folder}{samp}/emapper_annotations.gz")
+    eggnog_data.to_csv(f"{data_folder}{samp}/emapper_annotations.tsv", sep="\t")
+    # Map ,download and process MAGs
+    mag_list = sample_meta.spire_id.tolist()
+    for mag in mag_list:
+        urllib.request.urlretrieve(
+            f"https://spire.embl.de/download_file/{mag}",
+            f"{data_folder}{samp}/mags/{mag}.fa.gz",
+        )
+        # Print reconstruction command
+        command = f"carve --dna {data_folder}{samp}/mags/{mag}.fa.gz --egg {data_folder}{samp}/emapper_annotations.tsv -o {data_folder}{samp}/reconstructions/{mag}.xml"
+        print(command)
 
 
 def main():
@@ -34,10 +56,13 @@ def main():
     list_of_samples = study_meta.sample_id.tolist()
     for sample in list_of_samples:
         sample_folder = f"{study_folder}/{sample}/"
+        mag_folder = f"{sample_folder}/mags/"
+        model_folder = f"{sample_folder}/reconstructions/"
         os.makedirs(sample_folder, exist_ok=True)
-        sam = requests.get(f"https://spire.embl.de/api/sample/{sample}?format=tsv").text
-        sample_meta = pd.read_csv(io.StringIO(sam), sep="\t")
-        print(f"Sample: {sample}\tNo. of MAGs: {len(sample_meta.spire_id)}")
+        os.makedirs(mag_folder, exist_ok=True)
+        os.makedirs(model_folder, exist_ok=True)
+    with ThreadPoolExecutor as executor:
+        executor.map(process_sample, list_of_samples)
 
 
 if __name__ == "__main__":
