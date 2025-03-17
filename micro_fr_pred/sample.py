926e3cbdb30ae30c5d9f2306de7e3b42270c7e6b
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-03-17 12:05:45 +1000

    changes to sample

--- micro_fr_pred/sample.py
+++ micro_fr_pred/sample.py
@@ -1,4 +1,8 @@
 from study import Study
+import io
+import pandas as pd
+from util import clean_emapper_data
+import urllib.request
 import os
 import requests
 
@@ -27,11 +31,10 @@ class Sample:
         """
         self.id = id
         self.out_folder = out_folder
-        self.eggnog_data = None
-        self.mags = None
-        self.protein_bins = None
-        self.metadata = None
-        self.study: Study = None
+        self._eggnog_data = None
+        self._mags = None
+        self._protein_bins = None
+        self._metadata = None
 
         if out_folder:
             os.makedirs(out_folder, exist_ok=True)
@@ -40,13 +43,35 @@ class Sample:
             os.makedirs(out_folder)
 
     @property
-    def protein_bins(self):
-        return self.protein_bins
-
-    @property
     def eggnog_data(self):
-        return self.eggnog_data
+        if self._eggnog_data is None:
+            urllib.request.urlretrieve(
+                f"https://spire.embl.de/download_eggnog/{self.id}",
+                f"{self.out_folder}/emapper_annotations.gz",
+            )
+            eggnog_data = clean_emapper_data(
+                f"{self.out_folder}/emapper_annotations.gz"
+            )
+            eggnog_data.to_csv(f"{self.out_folder}/emapper_annotations.tsv", sep="\t")
+            self._eggnog_data = eggnog_data
+        return self._eggnog_data
 
     @property
-    def genomic_asembly(self):
-        return self.genomic_asembly
+    def mags(self):
+        if self._mags is None:
+            self._mags = self.metadata.spire_id.tolist()
+        return self._mags
+
+    @property
+    def protein_bins(self):
+        return self._protein_bins
+
+    @property
+    def metadata(self):
+        if self._metadata is None:
+            url = requests.get(
+                f"https://spire.embl.de/api/sample/{self.id}?format=tsv"
+            ).text
+            sample_meta = pd.read_csv(io.StringIO(url), sep="\t")
+            self._metadata = sample_meta
+        return self._metadata
