e93b906f87385b8888b7a49d7dd62d2ad3f37597
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-05-06 13:24:19 +1000

    trying to fix jug

--- scripts/study_reconstruction.py
+++ scripts/study_reconstruction.py
@@ -1,8 +1,9 @@
 import os
+import polars as pl
 import os.path as path
 import subprocess
 
-from jug import TaskGenerator, bvalue
+from jug import TaskGenerator, bvalue, iteratetask
 
 from spirepy import Study
 
@@ -17,16 +18,32 @@ def load_study(name: str):
 
 
 @TaskGenerator
+def generate_mag_list(study: Study):
+    genome_metadata = pl.read_csv(
+        path.join(data_folder, "spire_v1_genome_metadata.tsv.gz"), separator="\t"
+    )
+    return genome_metadata.filter(
+        genome_metadata["genome_id"].is_in(
+            bvalue(study).metadata["sample_id"].to_list()
+        )
+    )["genome_id"].to_list()
+
+
+@TaskGenerator
 def reconstruct_mag(mag: str, mag_folder: str, reconstruction_folder: str):
+    print(f"Starting reconstruction for {m}")
     input = path.join(mag_folder, f"{mag}.fa.gz")
     output = path.join(reconstruction_folder, f"{mag}.xml")
     command = f"carve --dna {input} --output {output} -i M9 -g M9 -v"
     subprocess.check_call(command, shell=True)
+    print(f"Finished reconstruction for {m}")
 
 
 study = load_study(study_name)
+mags = generate_mag_list(study)
+print(mags)
 
-for m in bvalue(study).mags["genome_id"].to_list():
+for m in bvalue(mags):
     reconstruct_mag(
         m,
         path.join(data_folder, study_name, "mags"),
@@ -34,4 +51,3 @@ for m in bvalue(study).mags["genome_id"].to_list():
             path.join(data_folder, study_name, "reconstructions"), exist_ok=True
         ),
     )
-    print(f"Finished reconstruction for {m}")
