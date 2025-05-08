e62f0908f38474f57bdea8f1ebc64805435126f3
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-05-08 14:56:21 +1000

    changed sample processing

--- scripts/sample_processing.py
+++ scripts/sample_processing.py
@@ -3,28 +3,29 @@ import os.path as path
 import subprocess
 import urllib
 
-import pandas as pd
 from jug import TaskGenerator, bvalue
 
-from micro_fr_pred import Sample, Study
+from spirepy import Study
 
 study_name = "Lloyd-Price_2019_HMP2IBD"
 data_folder = "/work/microbiome/users/areiasca/micro_fr_pred/data/"
 # data_folder = "/home/alexandre/Documents/projects/micro_fr_pred/data/"
 
 
-@TaskGenerator
-def process_sample(sample: Sample):
-    print("Sample:\t", sample.id)
-    manifest = sample.manifest
-    manifest.to_csv(path.join(s.out_folder, "sample_manifest.csv"))
-    return manifest
+# @TaskGenerator
+# def process_sample(sample: Sample):
+#     print("Sample:\t", sample.id)
+#     manifest = sample.manifest
+#     manifest.to_csv(
+#         path.join(data_folder, study_name, sample.id, "sample_manifest.csv")
+#     )
+#     return manifest
 
 
 @TaskGenerator
 def download_mag(mag, mag_folder):
     os.makedirs(mag_folder, exist_ok=True)
-    print("Started MAG:\t", mag)
+    print("Started downloading MAG:\t", mag)
     mag_out = path.join(mag_folder, f"{mag}.fa.gz")
     urllib.request.urlretrieve(
         f"https://spire.embl.de/download_file/{mag}",
@@ -35,6 +36,7 @@ def download_mag(mag, mag_folder):
 
 @TaskGenerator
 def run_carveme(mag, input, reconstruction_folder):
+    print("Started reconstructing MAG:\t", mag)
     os.makedirs(reconstruction_folder, exist_ok=True)
     output = path.join(reconstruction_folder, f"{mag}.xml")
     command = f"carve --dna {input} --output {output} -i M9 -g M9 -v"
@@ -42,31 +44,32 @@ def run_carveme(mag, input, reconstruction_folder):
     print("Finished processing MAG:\t", mag)
 
 
-@TaskGenerator
-def generate_study_manifest(study: Study):
-    list_manifests = []
-    for s in study.samples:
-        list_manifests.append(s.manifest)
-    manifest = pd.concat(list_manifests)
-    manifest.to_csv(path.join(study.folder, "study_manifest.csv"))
-    return manifest
+# @TaskGenerator
+# def generate_study_manifest(study: Study):
+#     list_manifests = []
+#     for s in study.samples:
+#         list_manifests.append(s.manifest)
+#     manifest = pd.concat(list_manifests)
+#     manifest.to_csv(path.join(study.folder, "study_manifest.csv"))
+#     return manifest
 
 
 @TaskGenerator
 def load_study(study_name: str):
-    study = Study(study_name, path.join(data_folder, study_name))
+    study = Study(study_name)
+    os.makedirs(path.join(data_folder, study_name), exist_ok=True)
+    print(f"Loaded study: {study.name}")
     return study
 
 
 study = load_study(study_name)
 
 for s in bvalue(study).samples:
-    manifest = process_sample(s)
-    mag_folder = path.join(s.out_folder, "mags")
-    reconstruction_folder = path.join(s.out_folder, "reconstructions")
-    for mag in bvalue(s.mags["genome_id"].tolist()):
+    print(f"Started Sample: {s}")
+    mag_folder = path.join(data_folder, study_name, s.id, "mags")
+    reconstruction_folder = path.join(data_folder, study_name, s.id, "reconstructions")
+    for mag in bvalue(s.mags["spire_id"].to_list()):
         magf = download_mag(mag, mag_folder)
         run_carveme(mag, magf, reconstruction_folder)
-    print(manifest)
 
-study_manifest = generate_study_manifest(study)
+# study_manifest = generate_study_manifest(study)
