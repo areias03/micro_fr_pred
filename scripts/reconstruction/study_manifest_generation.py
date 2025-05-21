5ddcce7cb607e720a08a7c57ca63c1fa89335d50
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-05-21 13:32:49 +1000

    added study simulation script

--- scripts/reconstruction/study_manifest_generation.py
+++ scripts/reconstruction/study_manifest_generation.py
@@ -1,20 +1,23 @@
+import glob
+import gzip
+import os.path as path
+
 import polars as pl
 from Bio import SeqIO
-import gzip
-import glob
-import os.path as path
 from spirepy import Study
 from spirepy.sample import Sample
+from tqdm import tqdm
 
 study_name = "Lloyd-Price_2019_HMP2IBD"
-data_folder = "/work/microbiome/users/areiasca/micro_fr_pred/data/"
+data_folder = "/home/alexandre/Documents/projects/micro_fr_pred/data/"
 
 
 def get_abundances(sample: Sample):
     abundances = {}
     mag_folder = path.join(data_folder, study_name, sample.id, "mags")
     depths = pl.read_csv(
-        f"/work/microbiome/global_data_spire/SPIRE/studies/{study_name}/psa_megahit/psb_metabat2/{sample.id}_aligned_to_{sample.id}.depths",
+        # f"/work/microbiome/global_data_spire/SPIRE/studies/{study_name}/psa_megahit/psb_metabat2/{sample.id}_aligned_to_{sample.id}.depths",
+        f"/home/alexandre/microbiome/global_data_spire/SPIRE/studies/{study_name}/psa_megahit/psb_metabat2/{sample.id}_aligned_to_{sample.id}.depths",
         separator="\t",
     )
     for f in sorted(glob.glob(path.join(mag_folder, "*.fa.gz"))):
@@ -72,6 +75,8 @@ def generate_manifest(sample: Sample):
     manifest = manifest.with_columns(
         (pl.col("abundance") / pl.col("abundance").sum()).alias("abundance")
     )
+    oname = path.join(data_folder, study_name, sample.id, "sample_manifest.csv")
+    manifest.write_csv(oname)
     return manifest
 
 
@@ -82,7 +87,7 @@ def load_study(study: str):
 def merge_partials(partials):
     study_manifest = pl.concat(partials, how="vertical")
     study_manifest = study_manifest.group_by("sample_id")
-    oname = path.join(data_folder, study_name, "study_manifest_linear.csv")
+    oname = path.join(data_folder, study_name, "study_manifest.csv")
     study_manifest.write_csv(oname)
     return oname
 
@@ -91,10 +96,14 @@ def main():
     study = load_study(study_name)
 
     manifest_list = []
-    for s in study.samples:
-        manifest = generate_manifest(s)
-        manifest_list.append(manifest)
-
+    for s in tqdm(study.samples):
+        print(s.id)
+        try:
+            manifest = generate_manifest(s)
+            manifest_list.append(manifest)
+        except:
+            print("Passed")
+            pass
     merge_partials(manifest_list)
 
 
---
+++ scripts/simulation/study_simulation.py
@@ -0,0 +1,51 @@
+import os.path as path
+
+import pandas as pd
+
+# from micom.data import test_medium
+from micom.interaction import interactions, summarize_interactions
+from micom.qiime_formats import load_qiime_medium
+from micom.viz import (
+    plot_exchanges_per_taxon,
+    plot_focal_interactions,
+    plot_growth,
+    plot_mes,
+)
+from micom.workflows import build, grow
+# from micom.workflows import complete_community_medium
+
+study_name = "Lloyd-Price_2019_HMP2IBD"
+# data_folder = "/home/alexandre/Documents/projects/micro_fr_pred/data/"
+data_folder = "/work/microbiome/users/areiasca/micro_fr_pred/data/"
+
+
+def main():
+    out_folder = path.join(data_folder, study_name, "simulations")
+    tax = pd.read_csv(path.join(data_folder, study_name, "study_manifest.csv"))
+    manifest = build(
+        taxonomy=tax, out_folder=out_folder, model_db=None, cutoff=0.0001, threads=32
+    )
+    # Western diet medium
+    medium = load_qiime_medium("../data/western_diet_gut.qza")
+    res = grow(
+        manifest,
+        model_folder=out_folder,
+        medium=medium,
+        tradeoff=1.0,
+        threads=32,
+    )
+    res.save(path.join(out_folder, "simulation_results.csv.zip"))
+    plot = plot_growth(res, filename=path.join(out_folder, "growth_rates.html"))
+    full = interactions(res, taxa=None, threads=32)
+    full.to_csv(path.join(out_folder, "interactions.csv"))
+    summary = summarize_interactions(full)
+    summary.to_csv(path.join(out_folder, "interactions_summary.csv"))
+    plot = plot_focal_interactions(
+        res, taxon=None, filename=path.join(out_folder, "niche.html")
+    )
+    plot = plot_mes(res, filename=path.join(out_folder, "mes.html"))
+    plot = plot_exchanges_per_taxon(res, filename=path.join(out_folder, "niche.html"))
+
+
+if __name__ == "__main__":
+    main()
