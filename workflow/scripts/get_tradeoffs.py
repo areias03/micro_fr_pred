395de664d74d063f7381ee0db91be4b9949796ab
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2025-07-24 09:20:14 +1000

    ADD tradeoff value analysis rule

--- workflow/scripts/get_tradeoffs.py
+++ workflow/scripts/get_tradeoffs.py
@@ -1,7 +1,8 @@
 import argparse
-import micom
+from micom.workflows import build
+from micom.workflows import tradeoff
+from micom.qiime_formats import load_qiime_medium
 import pandas as pd
-
 
 
 if __name__ == "__main__":
@@ -12,7 +13,14 @@ if __name__ == "__main__":
         help="Manifest file",
         type=str,
     )
-    parser.add_argument("-o", "--output", dest="output", help="output file")
+    parser.add_argument("-o", "--output", dest="output", help="output file prefix")
     args = parser.parse_args()
-    manifest = pd.read_csv(args.input)
-    with args.input as f:
+    data = pd.read_csv(args.input)
+    manifest = build(data, out_folder="models", cutoff=0.0001, threads=32)
+    medium = load_qiime_medium("../data/western_diet_gut.qza")
+    tradeoff_rates = tradeoff(
+        manifest, model_folder="models", medium=medium, threads=32
+    )
+    tradeoff_rates.groupby("tradeoff").apply(
+        lambda df: (df.growth_rate > 1e-6).sum()
+    ).reset_index().to_csv(args.ouput)
